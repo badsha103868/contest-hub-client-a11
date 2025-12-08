faa7400737d36523c61fb8c5823dc6b6f9cd61b6
badsha103868 <badshagolder5@gmail.com>
2025-12-08 12:58:59 +0600

    added dropdown problem solved

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Link, NavLink } from "react-router";
+import { NavLink } from "react-router";
+import ThemeToggle from "../../ThemeToggle/ThemeToggle";
+import logoImg from "../../../assets/logo.png";
 
 const Navbar = () => {
   const links = (
@@ -21,11 +23,12 @@ const Navbar = () => {
       </li>
     </>
   );
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-primary text-white shadow-sm">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <div className="dropdown z-50 text-black">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden z-50">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -33,7 +36,6 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-             
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -42,22 +44,43 @@ const Navbar = () => {
               />
             </svg>
           </div>
+
           <ul
-            tabIndex="-1"
+            tabIndex={-1}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             {links}
           </ul>
         </div>
-        <span className="btn btn-ghost text-xl"></span>
+
+        {/* Logo */}
+        <div className="flex items-center gap-1">
+          <img
+            className="w-[30px] h-[30px] md:w-[35px] md:h-[35px] rounded-full"
+            src={logoImg}
+            alt="Logo"
+          />
+          <h3 className="font-medium md:font-semibold lg:font-bold text-xl">
+            Contest Hub
+          </h3>
+        </div>
       </div>
+
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
+
       <div className="navbar-end">
-        <Link className="btn btn-primary text-black mx-4" to="/rider">
-          Be a Rider
-        </Link>
+        <div className="flex items-center gap-3">
+          <div className="w-10 mr-2 h-10 rounded-full overflow-hidden border">
+            <img
+              src="https://i.ibb.co/Y0WHx8Y/default-avatar.png"
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: { extend: {} },
+module.exports = {
+  content: ["./index.html", "./src//*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [require("daisyui")],
-  daisyui: { themes: ["light","dark"] },
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 };
