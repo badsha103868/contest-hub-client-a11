fd86b7fe3ecaad2c9c647ab15ad4a52279a900bc
badsha103868 <badshagolder5@gmail.com>
2025-12-08 14:16:10 +0600

    added firebase login, register,log out, and firebase authentication implement

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import { auth } from "../Firebase/firebase.init";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
-const AuthProvider = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  // user state
+  const [user, setUser] = useState(null);
+  // loading state
+  const [loading, setLoading] = useState(true);
+
+  // register
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //  login
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // log out
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  // update profile
+  const updateUser = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  // forget password
+  const forgetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  // google sign in
+  const googleSignIn = (googleProvider) => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // onAuthStateChanged observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authData = {
+    user,
+    setUser,
+    createUser,
+    signIn,
+    googleSignIn,
+    loading,
+    setLoading,
+    logOut,
+    updateUser,
+    forgetPassword,
+  };
+
+  return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../Context/AuthProvider';
+import { Navigate, useLocation } from 'react-router';
+import Loading from '../Pages/Loading/Loading';
 
-const PrivateRoute = () => {
-  return (
-    <div>
-      private Route
-    </div>
-  );
+
+
+const PrivateRoute = ({children}) => {
+  
+  const {user , loading} = use(AuthContext)
+  const location = useLocation()
+  console.log(location)
+
+   if(loading){
+        return <Loading></Loading>
+   }
+    
+  if(user){
+    return children;
+  }
+
+  return <Navigate state={location?.pathname} to='/auth/login'></Navigate>
+     
+  
 };
 
 export default PrivateRoute;
